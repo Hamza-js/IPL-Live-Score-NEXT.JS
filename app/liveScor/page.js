4ebed8dc3b06f74586996d0d0085165b385c25fe
Hamza-js <mhamzaawanmp1@gmail.com>
2023-04-13 05:29:45 -0700

    b

--- app/liveScor/page.js
+++ app/liveScor/page.js
@@ -1,11 +1,46 @@
-import React from "react";
+"use client";
+
+import { baseURL } from "@<components>/lib/baseURL";
+import axios from "axios";
+import React, { useEffect } from "react";
+const getLiveScore = async () => {
+  try {
+    console.log("hello");
+    const fetchData = await axios.get(baseURL, {
+      headers: {
+        accept: "*/*",
+      },
+    });
+    console.log(fetchData.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const liveScor = () => {
+  useEffect(() => {
+    const getLiveScore = async () => {
+      try {
+        console.log("hello");
+        const fetchData = await axios.get(baseURL, {
+          headers: {
+            accept: "*/*",
+          },
+        });
+        console.log(fetchData.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getLiveScore();
+  }, []);
+
   return (
     <div className="h-screen w-screen bg-[#F5F5F5] justify-center flex items-center">
       <div className="flex items-center flex-col max-w-[700px]  w-full h-full bg-white">
-      <div className="h-[60px] sm:h-[50px] w-full bg-purple-600 shadow-sm text-white flex items-center justify-center font-bold text-lg">
-          <p>Live Scores</p>
+        <div className="h-[60px] sm:h-[50px] w-full bg-purple-600 shadow-sm text-white flex items-center justify-center font-bold text-lg">
+          <p onClick={getLiveScore}>Live Scores</p>
         </div>
         <div className="px-8 pt-8 font-semibold text-black flex flex-col justify-center items-center w-full sm:w-[550px]">
           <p>Indian Premier League 2023</p>
@@ -18,9 +53,9 @@ const liveScor = () => {
               <p className="text-black text-xl font-normal">187/5</p>
               <p className="text-black text-[10px] font-normal">(19.4)</p>
             </div>
-
+            <p className="mt-5 text-gray-500">VS</p>
             <div className="flex flex-col items-center">
-            <p className="text-black text-lg font-normal hover:underline hover:cursor-pointer">
+              <p className="text-black text-lg font-normal hover:underline hover:cursor-pointer">
                 Rajasthan Royals
               </p>
               <p className="text-black text-xl font-normal">187/5</p>
